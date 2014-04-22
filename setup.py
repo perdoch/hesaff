9b22460394e1f0fa6c661aeee37cc4b7edbb6958
Jon Crall <crallj@rpi.edu>
2014-04-22 15:48:34 -0400

    wip

--- setup.py
+++ setup.py
@@ -14,30 +14,32 @@ from os.path import join, dirname
 def read(fname):
     return open(join(dirname(__file__), fname)).read()
 
-subprocess.call(['mkdir', 'build'])
 
-os.chdir('build')
+if __name__ == '__main__':
+    subprocess.call(['mkdir', 'build'])
 
-if subprocess.call(['cmake', '-G', 'Unix Makefiles', '..']):
-    subprocess.call(['make'])
+    os.chdir('build')
 
-os.chdir('..')
+    if subprocess.call(['cmake', '-G', 'Unix Makefiles', '..']):
+        subprocess.call(['make'])
 
-setup(
-    name='pyhesaff',
-    version='',
-    author='',
-    author_email='',
-    description=('Routines for computation of hessian affine keypoints in images.'),
-    license='',
-    keywords='',
-    url='https://github.com/Erotemic/hesaff',
-    packages=['build', 'pyhesaff', 'tests'],
-    package_data={'build': ['*.so']},
-    long_description=read('README'),
-    classifiers=[
-        '',
-        '',
-        '',
-    ],
-)
+    os.chdir('..')
+
+    setup(
+        name='pyhesaff',
+        version='',
+        author='',
+        author_email='',
+        description=('Routines for computation of hessian affine keypoints in images.'),
+        license='',
+        keywords='',
+        url='https://github.com/Erotemic/hesaff',
+        packages=['build', 'pyhesaff', 'tests'],
+        package_data={'build': ['*.so']},
+        long_description=read('README'),
+        classifiers=[
+            '',
+            '',
+            '',
+        ],
+    )
