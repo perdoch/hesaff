fd02dafd51dc57de0b152c67eb3c8865c4d44ff3
Jon Crall <crallj@rpi.edu>
2014-04-22 15:43:01 -0400

    style changes

--- setup.py
+++ setup.py
@@ -1,33 +1,37 @@
-import os
 from setuptools import setup
+import os
 import subprocess
 
 #using "http://pythonhosted.org/an_example_pypi_project/setuptools.html"'s setup.py as a template
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-subprocess.call(["mkdir","build"])
-os.chdir("build")
-if subprocess.call(["cmake","-G","Unix Makefiles",".."]):
-    subprocess.call(["make"])
-os.chdir("..")
+subprocess.call(['mkdir', 'build'])
+
+os.chdir('build')
+
+if subprocess.call(['cmake', '-G', 'Unix Makefiles', '..']):
+    subprocess.call(['make'])
+
+os.chdir('..')
 
 setup(
-    name = "pyhesaff",
-    version = "",
-    author = "",
-    author_email = "",
-    description = ("Routines for computation of hessian affine keypoints in images."),
-    license = "",
-    keywords = "",
-    url = "https://github.com/Erotemic/hesaff",
+    name='pyhesaff',
+    version='',
+    author='',
+    author_email='',
+    description=('Routines for computation of hessian affine keypoints in images.'),
+    license='',
+    keywords='',
+    url='https://github.com/Erotemic/hesaff',
     packages=['build', 'pyhesaff', 'tests'],
     package_data={'build': ['*.so']},
     long_description=read('README'),
     classifiers=[
-        "",
-        "",
-        "",
+        '',
+        '',
+        '',
     ],
 )
