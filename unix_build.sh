b7796992ab43f9889a55bf42b42746468f7ba631
Jon Crall <crallj@rpi.edu>
2014-06-25 14:31:48 -0400

    wip

--- unix_build.sh
+++ unix_build.sh
@@ -2,6 +2,9 @@
 mkdir build
 cd build
 
-cmake -G "Unix Makefiles" .. && make
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -G "Unix Makefiles" .. && make
+else
+    cmake -G "Unix Makefiles" .. && make
 
 cp libhesaff* ../pyhesaff
