f881559222f6ce04f0f86b64bdd3a8b650fe08ce
joncrall <erotemic@gmail.com>
2014-11-26 14:23:43 -0500

    Added noinjects and fixed relative imports'

--- pyhesaff/__init__.py
+++ pyhesaff/__init__.py
@@ -3,6 +3,10 @@ from __future__ import absolute_import, print_function, division
 
 __version__ = '1.1.0.dev1'
 
-from ._pyhesaff import (extract_vecs, detect_kpts, detect_kpts_list,
-                        adapt_rotation, adapt_scale, kpts_dtype, vecs_dtype,
-                        KPTS_DIM, DESC_DIM, __LIB_FPATH__, HESAFF_CLIB)
+import utool as ut
+ut.noinject(__name__, '[pyhesaff.__init__]')
+
+
+from pyhesaff._pyhesaff import (extract_vecs, detect_kpts, detect_kpts_list,
+                                adapt_rotation, adapt_scale, kpts_dtype, vecs_dtype,
+                                KPTS_DIM, DESC_DIM, __LIB_FPATH__, HESAFF_CLIB)
