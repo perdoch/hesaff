b8ee6ff5ecd8fdcb044743a849d40e827ac82f37
joncrall <erotemic@gmail.com>
2015-01-27 13:12:12 -0500

    wip

--- unix_build.sh
+++ unix_build.sh
@@ -35,14 +35,27 @@ python -c "import utool as ut; print('keeping build dir' if ut.get_argflag('--no
 mkdir build
 cd build
 
+echo "$OSTYPE"
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -G "Unix Makefiles" ..  || { echo "FAILED CMAKE CONFIGURE" ; exit 1; }
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    echo "USE MINGW BUILD INSTEAD" ; exit 1
+    export INSTALL32="c:/Program Files (x86)"
+    export HESAFF_INSTALL="$INSTALL32/Hesaff"
+    echo "INSTALL32=$INSTALL32"
+    echo "HESAFF_INSTALL=$HESAFF_INSTALL"
+    cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX="$HESAFF_INSTALL" -DOpenCV_DIR="$INSTALL32/OpenCV" .. || { echo "FAILED CMAKE CONFIGURE" ; exit 1; }
 else
     cmake -G "Unix Makefiles" ..  || { echo "FAILED CMAKE CONFIGURE" ; exit 1; }
 fi
 
-export NCPUS=$(grep -c ^processor /proc/cpuinfo)
-make -j$NCPUS || { echo "FAILED MAKE" ; exit 1; }
+if [[ "$OSTYPE" == "msys"* ]]; then
+    make || { echo "FAILED MAKE" ; exit 1; }
+else
+    export NCPUS=$(grep -c ^processor /proc/cpuinfo)
+    make -j$NCPUS || { echo "FAILED MAKE" ; exit 1; }
+fi
 
 cp libhesaff* ../pyhesaff --verbose
 cd ..
