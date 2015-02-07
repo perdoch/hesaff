a1d91cd54ae7ad59ed14d9fc49f7b4ff0aa2d80b
joncrall <erotemic@gmail.com>
2015-02-07 17:01:05 -0500

    updated build command

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@
 from __future__ import absolute_import, division, print_function
 from utool import util_cplat
 from utool import util_setup
+import utool as ut
 from setuptools import setup
 
 
-def build_command():
-    """ Build command run by utool.util_setup """
-    print('Running custom build')
-    if util_cplat.WIN32:
-        util_cplat.shell('mingw_build.bat')
-    else:
-        util_cplat.shell('./unix_build.sh')
+#def build_command():
+#    """ Build command run by utool.util_setup
+#    TODO: can use ut.std_build_cmd instead
+#    """
+#    print('Running custom build')
+#    if util_cplat.WIN32:
+#        util_cplat.shell('mingw_build.bat')
+#    else:
+#        util_cplat.shell('./unix_build.sh')
 
 
 URL_LIST = [
@@ -38,7 +41,7 @@ if __name__ == '__main__':
         author_email='erotemic@gmail.com',
         install_requires=INSTALL_REQUIRES,
         package_data={'build': util_cplat.get_dynamic_lib_globstrs()},
-        build_command=build_command,
+        build_command=ut.std_build_command,
         setup_fpath=__file__,
     )
     kwargs = util_setup.setuptools_setup(**setup_dict)
