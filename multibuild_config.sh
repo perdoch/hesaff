55ac97496e1a6021d0272c669c4b39af56c3d11a
Jon Crall <jon.crall@kitware.com>
2019-07-06 13:58:29 -0400

    wip

--- multibuild_config.sh
+++ multibuild_config.sh
@@ -67,11 +67,11 @@ function pre_build {
   if [ -n "$IS_OSX" ]; then
     echo "skip pip prebuild"
 
-    $PYTHON -m pip install scikit-build ubelt cmake ninja -U
-
     # https://medium.com/@nuwanprabhath/installing-opencv-in-macos-high-sierra-for-python-3-89c79f0a246a
     # Probably need to install opencv before running install
     brew install opencv
+
+    $PYTHON -m pip install scikit-build ubelt cmake ninja -U --user
 
   else
     echo "Running for linux"
