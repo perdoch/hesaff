bca41c465412872e60dc26e58045e331d4494a1a
joncrall <jon.crall@kitware.com>
2022-06-16 18:47:11 -0400

    hack

--- build_wheels.sh
+++ build_wheels.sh
@@ -3,5 +3,6 @@ __doc__="""
 SeeAlso:
     pyproject.toml
 """
+# python -m build /project --wheel --outdir=/tmp/cibuildwheel/built_wheel --config-setting=-v
 #pip wheel -w wheelhouse .
 cibuildwheel --config-file pyproject.toml --platform linux --arch x86_64
